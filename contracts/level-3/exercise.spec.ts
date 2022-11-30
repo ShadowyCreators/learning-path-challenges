0b920eb051f035206fed1b8be89dbf9f5b2971c4
PaoloRollo <paolo.rollo1997@gmail.com>
2022-11-30 14:47:12 +0100

    feat: add level-3 contracts tests.

---
+++ contracts/level-3/exercise.spec.ts
@@ -0,0 +1,34 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { Attack__factory, Vulnerable__factory, Attack, Vulnerable } from "../../typechain-types/index";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+/**
+ * NOTE: Do not touch. This file is used for testing purposes.
+ */
+
+describe("[Contracts] Level 3", () => {
+  describe("Attack Contract", () => {
+    let attack: Attack, vulnerable: Vulnerable, attacker: SignerWithAddress, user: SignerWithAddress;
+
+    before(async () => {
+      [attacker, user] = await ethers.getSigners();
+
+      vulnerable = await new Vulnerable__factory(user).deploy();
+      const depositTx = await vulnerable.deposit({ value: 2 });
+      await depositTx.wait();
+      expect(await vulnerable.balances(user.address)).eq(2);
+      expect(await ethers.provider.getBalance(vulnerable.address)).eq(2);
+      attack = await new Attack__factory(attacker).deploy(vulnerable.address);
+      expect((await attack.functions.vulnerableContract())[0]).eq(vulnerable.address);
+    })
+
+    it('Attacking user must steal ETH from the vulnerable contract', async () => {
+        const attackerBalance = await ethers.provider.getBalance(attacker.address);
+        const attackTx = await attack.attack();
+        await attackTx.wait();
+        const afterAttackBalance = await ethers.provider.getBalance(attacker.address);
+        expect(afterAttackBalance).greaterThan(attackerBalance)
+    })
+  });
+});
